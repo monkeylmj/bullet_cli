8d0533337783ae3dcdbdda761914427a457cd047
monkeylmj <leaderlmj@gmail.com>
2023-04-16 15:55:37 +0800

    feat: 补充三方依赖.

--- setup.py
+++ setup.py
@@ -1,10 +1,24 @@
+import os
 from setuptools import setup, find_packages
+
+def _process_requirements():
+    packages = open('requirements.txt').read().strip().split('\n')
+    requires = []
+    for pkg in packages:
+        if pkg.startswith('git+ssh'):
+            return_code = os.system('pip install {}'.format(pkg))
+            assert return_code == 0, 'error, status_code is: {}, exit!'.format(return_code)
+        else:
+            requires.append(pkg)
+    return requires
 
 setup(
     name='bullet_cli',
     version='0.0.1',
     author="monkeyliu",
+    author_email="mrlmj@qq.com",
     description='watch stock in Terminal',
+    install_requires=_process_requirements(),
     packages=find_packages('src'),
     package_dir={'': 'src'},
     include_package_data=True,
